bb56b6212834f3bced027ae00c63f109144db195
Ethan <mrxone2025@protonmail.com>
2024-02-03 09:50:26 -0600

    Post-recording changes

--- Curse Word Censor/bot.py
+++ Curse Word Censor/bot.py
@@ -1,31 +1,32 @@
+import discord
+import responses as r
 from openai import OpenAI
 from dotenv import load_dotenv
-import discord
 import os
-import responses as r
 
 def run_bot():
     load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    client = OpenAI()
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
     discord_client = discord.Client(intents=intents)
+    openai_client = OpenAI()
 
     @discord_client.event
     async def on_ready():
-        print(f"{discord_client.user} is online")
+        print(f'{discord_client.user} is running properly!')
 
     @discord_client.event
     async def on_message(msg):
         if msg.author == discord_client.user:
             return
         
-        response = r.create_response(client, msg.content)
+        response = r.create_response(openai_client, msg.content)
+
         if response == 'none':
             return
-
+        
         await msg.delete()
-        await msg.channel.send(f"Your message has been filtered: {response}")
+        await msg.channel.send(f'Your message has been filtered for bad language: {response}')
 
-    discord_client.run(TOKEN)
+    discord_client.run(DISCORD_TOKEN)
--- Curse Word Censor/responses.py
+++ Curse Word Censor/responses.py
@@ -1,12 +1,10 @@
 def create_response(client, user_message):
     response_list = [
-    {"role": "system", 
-     "content": "You will be provided with statements. Detect all curse words and replace them with nicer words. If there are no curse words, simply reply with 'none'."}
+        {"role": "system",
+         "content": "You will be provided with statements. Detect all curse words and replace them with nicer words. If there are no curse words, simply reply with 'none'."},
+         {"role": "user",
+          "content": user_message}
     ]
-
-    response_list.append({"role": "user", 
-                        "content": user_message})
-
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
